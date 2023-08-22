d72895a36d92464600b8adf49ef40442344e965f
Ilya <124806482+Perekhod@users.noreply.github.com>
2023-08-22 14:35:05 +0500

    Update server chat Perekhod.cpp
    update. plus db 22.08

--- server chat Perekhod.cpp
+++ server chat Perekhod.cpp
@@ -1,177 +1,193 @@
 #include <SFML/Network.hpp>
 #include <algorithm>
+#include <iostream>
 #include <vector>
-#include "user.h"
+#include <string>
 #include "message.h"
+#include "db.h"
 
 int main()
 {
-	std::cout << "Server is running on:" << sf::IpAddress::getLocalAddress() << std::endl;
+	std::cout << "Server is running on: " << sf::IpAddress::getLocalAddress() << std::endl;
 
-	sf::TcpListener		listener;
-	sf::SocketSelector	selector;
-	std::vector<User*>	clients;
-
-	listener.listen(666);
+	
+	sf::TcpListener listener;
+	sf::SocketSelector selector;
+	
+	DB db;
+		
+	std::vector<std::pair<std::string, sf::TcpSocket *>> clients;
+	
+	listener.listen(55001);
 	selector.add(listener);
-
-	while (true)
+	
+	while(true)
 	{
-		if (selector.wait())									//	Part responsible for creating a new user
-		{														//	And username verification
-			if (selector.isReady(listener))
+		if(selector.wait())
+		{
+			if(selector.isReady(listener))
 			{
-				sf::TcpSocket* socket = new sf::TcpSocket;
+				sf::TcpSocket *socket = new sf::TcpSocket;
 				listener.accept(*socket);
-
+				
 				sf::Packet packet;
 				std::string log, pas;
-				socket->receive(packet);
+				socket -> receive(packet);
 				packet >> log >> pas;
-
-				User* usr = new User(log, pas);
-				usr->set_socket(socket);
-
-				for (int r = 0; r <= (clients.size() - 1); r++)
+				
+				for(int r = 0; r <= (clients.size() -1); r++)
 				{
 					if (clients.empty())
 					{
-						clients. push_back(usr);
+						clients.push_back(std::make_pair(log, socket));
 						selector.add(*socket);
-
-						packet.	 clear();
-
+						
+						db.add_user(log,pas);
+						
+						packet.clear();
+						
 						Message message;
 						message._sender = "server";
 						message._recipient = log;
 						message._text_message = "good";
-
-						packet.	clear();
+						
+						packet.clear();
 						packet << message;
-						socket->send(packet);
-
-						packet.	clear();
+						socket -> send(packet);
+						
+						packet.clear();
 						message.clear();
-
-						usr		= nullptr;
-						socket	= nullptr;
+						
+						socket = nullptr;
+						
 						break;
 					}
-
-					if (clients[r]->get_name() == usr->get_name())
+					
+					if(clients[r].first == log)
 					{
 						std::cout << "Client exist\n";
 						Message message;
-						message._sender			= "server";
-						message._recipient		= log;
-						message._text_message	= "Exist";
-
-						packet.	clear();
+						message._sender = "server";
+						message._recipient = log;
+						message._text_message = "Exist";
+						
+						packet.clear();
 						packet << message;
-						socket->send(packet);
-
-						packet.	clear();
+						socket -> send(packet);
+						
+						packet.clear(); 
 						message.clear();
 
-						selector.remove(*socket);
 						delete socket;
-						delete usr;
+						
 						break;
 					}
-
-					if ((r == (clients.size() - 1)) && ((clients[r]->get_name()) != (usr->get_name())))
+					
+					if( (r == (clients.size() -1)) && ( (clients[r].first) != (log) ) )
 					{
-						clients.push_back(usr);
+						clients.push_back(std::make_pair(log, socket));
 						selector.add(*socket);
-
+						
+						db.add_user(log,pas);
+						
 						packet.clear();
-
+						
 						Message message;
-						message._sender			= "server";
-						message._recipient		= log;
-						message._text_message	= "good";
-
+						message._sender = "server";
+						message._recipient = log;
+						message._text_message = "good";
+						
 						packet.clear();
 						packet << message;
-						socket->send(packet);
-
+						socket -> send(packet);
+						
 						packet.clear();
 						message.clear();
-
-						usr		= nullptr;
-						socket	= nullptr;
+						
+						socket = nullptr;
+						
 						break;
 					}
-				}
+				}				
 			}
-			else														//	User request check part
+		//конец 'создания(проверки) пользователя'
+			
+			//часть проверки запросов от пользователя
+			else
 			{
-				for (int i = 0; i < clients.size(); i++)
+				for(int i = 0; i < clients.size(); i++)
 				{
-					if (selector.isReady(*(clients[i]->get_socket())))
+					if(selector.isReady(*(clients[i].second)))
 					{
 						sf::Packet packet;
-						if (clients[i]->get_socket()->receive(packet) == sf::Socket::Done)
+						if(clients[i].second -> receive(packet) == sf::Socket::Done)
 						{
 							Message message;
 							packet >> message;
 							if (message._text_message == "list_of_users")
 							{
-								packet.	clear();
+								packet.clear();
 								message.clear();
 
-								for (int j = 0; j < clients.size(); j++)
-								{
-									message._text_message += clients[j]->get_name() + ' ';
-								}
-
-								message._sender		= "server";
-								message._recipient	= clients[i]->get_name();
+								message._text_message = db.list_of_users(clients[i].first);
+								
+								message._sender = "server";
+								message._recipient = clients[i].first;
 
 								packet << message;
-								(*clients[i]).get_socket()->send(packet);
+								clients[i].second -> send(packet);
 								packet.clear();
 								message.clear();
 							}
-
-							if (message._recipient == "all")
+							
+							if(message._recipient == "all")
 							{
 								packet << message;
-
-								for (int i = 0; i < clients.size(); i++)
+								
+								db.add_mess(message._sender, "all", message._text_message);
+								
+								for(int i = 0; i < clients.size(); i++)
 								{
-									if (clients[i]->get_name() != message._sender) (*clients[i]).get_socket()->send(packet);
+									if(clients[i].first != message._sender) clients[i].second -> send(packet);
 								}
 
-								packet.	clear();
+								packet.clear();
 								message.clear();
 							}
-
-							if (message._text_message == "delete")				
-							{													//	Cleans the vector of offline users
-								for (auto iter = clients.begin(); iter < clients.end(); iter++)
+							
+							if (message._text_message == "delete")
+							{//очищает вектор от офлайн юзеров
+								for(auto iter = clients.begin(); iter < clients.end(); iter++)
 								{
-									if ((*iter)->get_name() == message._sender)
+									if((*iter).first == message._sender)
 									{
-										selector.remove(*((*iter)->get_socket()));
+										db.del_user((*iter).first);
+										selector.remove( (*(*iter).second));
 										clients.erase(std::remove(clients.begin(), clients.end(), *iter), clients.end());
-
-										packet.	clear();
+										
+										packet.clear();
 										message.clear();
+										
 										break;
 									}
 								}
+							//Конец очистки
 							}
+							
 							else
 							{
-								for (int i = 0; i < clients.size(); i++)
+								for(int i = 0; i < clients.size(); i++)
 								{
-									if (message._recipient == clients[i]->get_name())
+									if(message._recipient == clients[i].first)
 									{
-										packet.	clear();
+										packet.clear();
 										packet << message;
-										(*clients[i]).get_socket()->send(packet);
-										packet.	clear();
+										
+										db.add_mess(message._sender, message._recipient, message._text_message);
+										
+										clients[i].second -> send(packet);
+										
+										packet.clear();
 										message.clear();
 										break;
 									}
@@ -181,7 +197,10 @@ int main()
 					}
 				}
 			}
-		}
+			//Конец проверки запросов пользователя
+		}		
+
 	}
+			
 	return 0;
 }
